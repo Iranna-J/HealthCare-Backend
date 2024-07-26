c4cc99dd5644f2b8b0dc94489aa58b551fab9764
Madhuri144 <madhurimanganahalli>
2024-07-26 10:39:51 +0530

    first commit

---
+++ src/main/java/com/HealthCareBack/HealthCareBackApplication.java
@@ -0,0 +1,13 @@
+package com.HealthCareBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareBackApplication.class, args);
+	}
+
+}
